470d4149f673fdf7a1e1508de41aac8d87b6543a
aicha-azr <aichaazeroual03@gmail.com>
2024-03-15 13:50:11 +0100

    landing page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -6,12 +7,27 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    theme: {
+      extend: {
+        fontFamily: {
+          'BodyFont': ['Montserrat', 'sans-serif'],
+          'HeadingFont': ['Playfair Display', 'serif'],
+        },
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        "black-maron": "#291f1d",
+        "bleu-ciel": "#c2d2e1",
+        "orange": "#e35733",
+        "jaune": "#fbbc3b",
+        "blanc": "#fbf7eb"
+      }
     },
   },
   plugins: [],
