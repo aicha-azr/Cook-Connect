60a1d1716aebbcef0a739583e0b9cb400c5a533d
aicha-azr <aichaazeroual03@gmail.com>
2024-08-08 19:12:23 +0100

    updating the sign in page

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -38,7 +38,7 @@ function setCookie(name, value, days) {
 
 
   return (<>
-    <div className=" inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-blanc py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
+    <div className=" inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-blanc py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg lg:mt-1">
         <div className="px-4">
           <div className="flex items-center justify-between">
             <div className="flex shrink-0">
@@ -57,10 +57,10 @@ function setCookie(name, value, days) {
                 className="hidden items-center justify-center rounded-xl bg-bleu-ciel px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
                 href="/sign-in"
               >
-                Login
+                Log in
               </a>
               <a
-                className="inline-flex items-center justify-center rounded-xl bg-jaune px-3 py-2 text-sm font-semibold text-blanc shadow-sm transition-all duration-150 hover:bg-bleu-ciel focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                className="inline-flex items-center justify-center rounded-xl bg-jaune px-3 py-2 text-sm font-semibold text-black-maron shadow-sm transition-all duration-150 hover:bg-bleu-ciel focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                 href="/sign-up"
               >
                 Sign up
@@ -77,36 +77,38 @@ function setCookie(name, value, days) {
 
 
 
-    <div className='flex justify-center mt-10 lg:mt-20 pt-2'>
-  <div className="flex items-center justify-center md:h-screen ">
-      <div className=" flex items-center w-full max-w-[400px] flex-col  p-4 md:-mt-32">
+    <div className='flex justify-center mt-10 lg:mt-0 '>
+  <div className="flex items-center justify-center  ">
+      <div className=" flex items-center w-full max-w-[400px] flex-col p-4">
        
           <div className="w-32 text-white flex  justify-center md:w-36">
-           <Image src={logo} width={400} height={400}/>
+           <Image src={logo} width={100} height={100}/>
           </div>
             
 <div
-    className="relative mx-auto mt-10 w-full max-w-md bg-bleu-ciel px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
-    <div className="w-full">
+    className="relative mx-auto mt-10 w-full max-w-md border border-bleu-ciel px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 rounded-xl sm:px-10 text-black-maron">
+    <div className="w-full flex flex-col gap-2">
         <div className="text-center">
             
-            <p className="mt-2 text-gray-500">Log in below to access your account</p>
+            <p className=" text-black">Log in below to access your account</p>
         </div>
             <form action="post" onSubmit={handleLogin}>
-                <div className="relative mt-6">
-                    <input type="email" name="email" id="email" placeholder="Email Address" className="peer rounded-md bg-blanc mt-1 w-full border-b-2 border-black-maron px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" autocomplete="NA" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
-                    <label for="email" className="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800">Email Address</label>
+                <div className="flex gap-0.5 flex-col">
+                   <label for="email" className="text-sm">Email Address</label>
+                    <input type="email" name="email" id="email" placeholder="Email Address" className="peer rounded-xl bg-blanc mt-1 w-full border-b-2 border-bleu-ciel px-1 focus:ring-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" autocomplete="NA" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+                   
                 </div>
-                <div className="relative mt-6">
-                    <input type="password" name="password" id="password" placeholder="Password" className="peer peer mt-1 bg-blanc rounded-md w-full border-b-2 border-black-maron px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none"  value={mot_de_passe} onChange={(e)=>setMot_de_passe(e.target.value)} required/>
-                    <label for="password" className="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800">Password</label>
+                <div className="flex gap-0.5 flex-col">
+                  <label for="password" className="text-sm">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Password" className="peer rounded-xl bg-blanc mt-1 w-full border-b-2 border-bleu-ciel px-1 focus:ring-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none"  value={mot_de_passe} onChange={(e)=>setMot_de_passe(e.target.value)} required/>
+                    
                 </div>
                 <div className="my-6">
-                    <button type="submit" className="w-full rounded-md bg-jaune px-3 py-4 text-black-maron font-bold focus:bg-gray-600 focus:outline-none">Log in</button>
+                    <button type="submit" className="w-full rounded-xl bg-black-maron px-3 py-2 text-jaune font-bold focus:bg-gray-600 focus:outline-none">Log in</button>
                 </div>
-                <p className="text-center text-sm text-gray-500">Don&#x27;t have an account yet?
+                <p className="text-center text-sm text-black-maron">Don&#x27;t have an account yet? 
                     <a href="/signin"
-                        className="font-semibold text-gray-600 hover:underline focus:text-orange focus:outline-none">Sign
+                        className="font-semibold text-gray-600 hover:underline text-orange focus:outline-none"> Sign
                         up
                     </a>
                 </p>
